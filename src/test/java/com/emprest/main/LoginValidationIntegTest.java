6796e1f973ea74a1f5c179e42d1334874b99f4ac
authis-sahadevan <authis.sahadevan@wipro.com>
2019-12-17 16:06:03 +0000

    FIxing rest assured service

--- src/test/java/com/emprest/main/LoginValidationIntegTest.java
+++ src/test/java/com/emprest/main/LoginValidationIntegTest.java
@@ -1,13 +1,36 @@
 package com.emprest.main;
 
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.contains;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginValidationIntegTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        RestAssuredMockMvc.webAppContextSetup(this.webApplicationContext);
+    }
 
     @Test
     public void myFirstRestAssuredTest()
@@ -18,9 +41,11 @@ public class LoginValidationIntegTest {
                 accept(ContentType.JSON).
                 body("{  \"username\": \"admin\", \"password\": \"admin\" }").
                 when().
-                post("http://localhost:5002/validateLogon").
+                post("http://localhost:"+port+"/validateLogon").
                 then().
                 contentType(ContentType.JSON).
-                body("statusMsg", equalTo("SUCCESS"));
+                //body(Matchers.contains("SUCCESS"));
+                        body("statusMsg", equalTo("SUCCESS"));
+
     }
 }
